01234b0e1f58d14534363b0f43ff91a08b8b7fe8
niekky <trungkiencfc@gmail.com>
2023-03-19 20:11:42 -0700

    - Reimplemented GPS_Get_ID, GPS_Load_Config - Added GPS_Transmit, GPS_Receive, GPS_Get_NavigatorData

--- gps/gps.c
+++ gps/gps.c
@@ -24,6 +24,95 @@
 /*******************************************************************************
 *                                                                              *
 * PROCEDURE:                                                                   * 
+* 		GPS_Transmit                                                           *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		Transmits a specified number of bytes over GPS                         *
+*                                                                              *
+*******************************************************************************/
+
+GPS_STATUS GPS_Transmit
+    (
+    void*       data_ptr,
+    size_t      data_size,
+    uint32_t    timeout
+    )
+{
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+HAL_StatusTypeDef gps_status;
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+/* Transmit byte */
+gps_status = HAL_UART_Transmit( &( GPS_HUART ),
+                                data_ptr,
+                                data_size,
+                                timeout );
+
+/* Return HAL status */
+if ( gps_status != HAL_TIMEOUT )
+    {
+    return GPS_OK;
+    }
+else
+    {
+    return GPS_TIMEOUT;
+    }
+    
+} /* GPS_Transmit */
+
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+* 		GPS_Receive                                                            *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		Receive bytes from the GPS                                             *
+*                                                                              *
+*******************************************************************************/
+
+GPS_STATUS GPS_Receive
+    (
+    void*       data_ptr,
+    size_t      data_size,
+    uint32_t    timeout
+    )
+{
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+HAL_StatusTypeDef gps_status;
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+/* Receive byte */
+gps_status = HAL_UART_Receive( &( GPS_HUART ),
+                                data_ptr,
+                                data_size,
+                                timeout );
+
+/* Return HAL status */
+if ( gps_status != HAL_TIMEOUT )
+    {
+    return GPS_OK;
+    }
+else
+    {
+    return GPS_TIMEOUT;
+    }
+
+} /* GPS_Receive */
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
 * 		GPS_Load_Config                                                        *
 *                                                                              *
 * DESCRIPTION:                                                                 * 
@@ -34,21 +123,24 @@
 
 void GPS_Load_Config()
 {
-    HAL_UART_Transmit_DMA( &( GPS_HUART )   ,
-                            configUBX,
-                            sizeof(configUBX) / sizeof(uint8_t) );
-    HAL_Delay(200);
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
 
-    HAL_UART_Transmit_DMA( &( GPS_HUART )   ,
-                            configNMEA,
-                            sizeof(configNMEA) / sizeof(uint8_t) );
-    HAL_Delay(200);
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
 
-    HAL_UART_Transmit_DMA( &( GPS_HUART )   ,
-                            setGNSS,
-                            sizeof(setGNSS) / sizeof(uint8_t) );
-    HAL_Delay(200);
-}
+GPS_Transmit( configUBX, sizeof(configUBX) / sizeof(uint8_t), HAL_DEFAULT_TIMEOUT );
+HAL_Delay(200);
+
+GPS_Transmit( configNMEA, sizeof(configNMEA) / sizeof(uint8_t), HAL_DEFAULT_TIMEOUT );
+HAL_Delay(200);
+
+GPS_Transmit( setGNSS, sizeof(setGNSS) / sizeof(uint8_t), HAL_DEFAULT_TIMEOUT );
+HAL_Delay(200);
+
+} /* GPS_Load_Config */
 
 /*******************************************************************************
 *                                                                              *
@@ -60,17 +152,54 @@ void GPS_Load_Config()
 *                                                                              *
 *******************************************************************************/
 
-GPS_STATUS GPS_Get_ID()
+GPS_STATUS GPS_Get_ID
+    (
+    uint8_t* data_ptr;
+    )
 {
-    HAL_StatusTypeDef hal_status = HAL_UART_Transmit_DMA(   &( GPS_HUART ),
-                                                            getDeviceID,
-                                                            sizeof( getDeviceID ) / sizeof( uint8_t ));
-    if ( hal_status != HAL_TIMEOUT )
-        {
-        return GPS_OK;
-        }
-    else
-        {
-        return GPS_TIMEOUT
-        }
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+GPS_STATUS gps_status;
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+gps_status = GPS_Transmit( getDeviceID, sizeof( getDeviceID ) / sizeof( uint8_t ), HAL_DEFAULT_TIMEOUT );
+gps_status = GPS_Receive( data_ptr, sizeof(data_ptr), HAL_DEFAULT_TIMEOUT );
+
+return gps_status;
+
+}
+
+/*******************************************************************************
+*                                                                              *
+* PROCEDURE:                                                                   * 
+* 		GPS_Get_NavigatorData                                                  *
+*                                                                              *
+* DESCRIPTION:                                                                 * 
+* 		Return GPS Navigator Data                                              *
+*                                                                              *
+*******************************************************************************/
+
+GPS_STATUS GPS_Get_NavigatorData
+    (
+    uint8_t* data_ptr
+    )
+{
+/*------------------------------------------------------------------------------
+ Local Variables
+------------------------------------------------------------------------------*/
+GPS_STATUS gps_status;
+
+/*------------------------------------------------------------------------------
+ API Function Implementation 
+------------------------------------------------------------------------------*/
+
+gps_status = GPS_Transmit( getNavigatorData, sizeof( getNavigatorData ) / sizeof( uint8_t ), HAL_DEFAULT_TIMEOUT );
+gps_status = GPS_Receive( data_ptr, sizeof(data_ptr), HAL_DEFAULT_TIMEOUT );
+
+return gps_status;
+
 }
